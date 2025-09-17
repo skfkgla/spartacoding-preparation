342a1f344731bb269321a7e7c3eadf787315483a
skfkgla <narahim.lee@gmail.com>
2025-09-17 21:54:50 +0900

    Spring을 제외한 Java와 Hibernate를 이용한 JPA 실습

---
+++ week1/jpa-core/src/main/java/com/sparta/entity/Memo.java
@@ -0,0 +1,25 @@
+package com.sparta.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
+public class Memo {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	// nullable: null 허용 여부
+	// unique: 중복 허용 여부 (false 일때 중복 허용)
+	@Column(name = "username", nullable = false, unique = true)
+	private String username;
+
+	// length: 컬럼 길이 지정
+	@Column(name = "contents", nullable = false, length = 500)
+	private String contents;
+}
