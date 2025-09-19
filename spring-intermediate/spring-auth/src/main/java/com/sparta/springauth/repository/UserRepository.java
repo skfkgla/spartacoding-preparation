0561ec5410521381aa352e65f9223e28eb683ccd
skfkgla <narahim.lee@gmail.com>
2025-09-19 14:33:57 +0900

    회원가입 구현

---
+++ spring-intermediate/spring-auth/src/main/java/com/sparta/springauth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.springauth.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.springauth.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
+
+	Optional<User> findByEmail(String email);
+}
