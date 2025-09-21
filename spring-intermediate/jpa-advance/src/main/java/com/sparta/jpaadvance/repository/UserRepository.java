f4f9d9a0c31e4d57b1c85135d080524fbe04c831
skfkgla <narahim.lee@gmail.com>
2025-09-21 22:11:28 +0900

    @OneToOne 단방향 테스트

---
+++ spring-intermediate/jpa-advance/src/main/java/com/sparta/jpaadvance/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.sparta.jpaadvance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.jpaadvance.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
