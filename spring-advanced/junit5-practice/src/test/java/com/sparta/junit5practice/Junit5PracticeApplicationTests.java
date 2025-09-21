1a8324f296f8480ffe626e40260aefec9cb04f6f
skfkgla <narahim.lee@gmail.com>
2025-09-22 03:07:28 +0900

    junit5 Test init commit

---
+++ spring-advanced/junit5-practice/src/test/java/com/sparta/junit5practice/Junit5PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.junit5practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
